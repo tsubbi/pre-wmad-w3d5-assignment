c703543baa6e5243f837ce952692c707f0a585cf
Jamie <jamie.yjchen@gmail.com>
2020-12-11 15:08:35 -0800

    Finish Log Array

--- logArray.js
+++ logArray.js
@@ -3,7 +3,9 @@
 let example = [1, 2, 3, 4, 'five', 6, 7];
 
 const logItems = function (arr) {
-
+    for (i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
 };
 
 logItems(example);
