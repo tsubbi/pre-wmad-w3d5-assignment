ead710071dc2a605d13a7f4fe28a27f5a51536c0
Jamie <jamie.yjchen@gmail.com>
2020-12-11 14:54:04 -0800

    Finish Log String and Numbers

--- logStringsAndNumbers.js
+++ logStringsAndNumbers.js
@@ -5,7 +5,13 @@
 let example = [1, 'dog', 3, 'eight', 5, 'car', 7, 8];
 
 const logStringsAndNumbers = function (arr) {
-
+    for (i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === "string") {
+            console.log(`${arr[i]} this is a ${typeof arr[i]}`);
+        } else {
+            console.log(arr[i]);
+        }
+    }
 };
 
 logStringsAndNumbers(example);
