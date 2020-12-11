f9800fb3ac110edbb03331186f47dca0a228da89
Jamie <jamie.yjchen@gmail.com>
2020-12-11 14:27:43 -0800

    Finish FizzBuzz

--- variableFizzBuzz.js
+++ variableFizzBuzz.js
@@ -8,7 +8,21 @@
 // if the end number is lower than the start, console.log("Improper Input");
 
 const fizzbuzz = function (start, end) {
-
+    if (start > end) {
+        console.log("Improper Input");
+        return;
+    } 
+    for (i = start; i <= end; i++) {
+        if (i % 15 === 0) {
+            console.log("fizzbuzz");
+        } else if (i % 5 === 0) {
+            console.log("fizz");
+        } else if (i % 3 === 0) {
+            console.log("buzz");
+        } else {
+            console.log(i);
+        }
+    }
 };
 
 
